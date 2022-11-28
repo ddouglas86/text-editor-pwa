fdc99f0daa48b588fec30846bb7abfa386774d15
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-11-27 22:50:02 -0600

    added plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -43,29 +43,12 @@ module.exports = () => {
             destination: path.join('assets', 'icons'),
           },
         ],
-      }),
+      }), 
     ],
 
     module: {
       rules: [
-        {
-          test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
-        },
-        {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env"],
-              plugins: [
-                "@babel/plugin-proposal-object-rest-spread",
-                "@babel/transform-runtime",
-              ],
-            },
-          },
-        }, 
+        
       ],
     },
   };
